fed48d056f8b00d344a1dd2520da4f635f3ebae7
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-01-28 01:22:04 +0100

    install next-pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: false,
+})
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig) 
