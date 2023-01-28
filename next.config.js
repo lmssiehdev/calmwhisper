2e15618a67ccd8660ef37cf35767878df1b47454
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-01-28 16:32:20 +0100

    install next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: false,
+  register: true
 })
 
 const nextConfig = {
--- public/sw.js
+++ public/sw.js
@@ -76,20 +76,12 @@ define(['./workbox-327c579b'], (function (workbox) { 'use strict';
     "cacheName": "start-url",
     plugins: [{
       cacheWillUpdate: async ({
-        request,
-        response,
-        event,
-        state
-      }) => {
-        if (response && response.type === 'opaqueredirect') {
-          return new Response(response.body, {
-            status: 200,
-            statusText: 'OK',
-            headers: response.headers
-          });
-        }
-        return response;
-      }
+        response: e
+      }) => e && "opaqueredirect" === e.type ? new Response(e.body, {
+        status: 200,
+        statusText: "OK",
+        headers: e.headers
+      }) : e
     }]
   }), 'GET');
   workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
