a3b15408abb5dcd6d1aff2f779e23dfbd535c9c8
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-03-03 21:44:10 +0100

    fix: delete unnecessary meta description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -16,7 +16,6 @@ export default function Head() {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-config" content="/icons/browserconfig.xml" />
