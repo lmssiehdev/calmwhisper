074f2fdec7fde146d5428c4205d70936c8efccfb
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-05-14 00:36:01 +0100

    Fix: sound not playing on ios (#10)
    closes #1

--- src/soundData.tsx
+++ src/soundData.tsx
@@ -10,83 +10,83 @@ export const soundData: ISoundData = [
   {
     name: "bird",
     icon: "/icons/bird.svg",
-    sound: "/sounds/birds.ogg",
+    sound: "/sounds/birds.mp3",
   },
   {
     name: "boat",
-    sound: "/sounds/boat.ogg",
+    sound: "/sounds/boat.mp3",
     icon: "/icons/boat.svg",
   },
 
   {
     name: "city",
-    sound: "/sounds/city.ogg",
+    sound: "/sounds/city.mp3",
     icon: "/icons/city.svg",
   },
 
   {
     name: "coffee-shop",
-    sound: "/sounds/coffee-shop.ogg",
+    sound: "/sounds/coffee-shop.mp3",
     icon: "/icons/coffee-shop.svg",
   },
 
   {
     name: "fireplace",
-    sound: "/sounds/fireplace.ogg",
+    sound: "/sounds/fireplace.mp3",
     icon: "/icons/fireplace.svg",
   },
 
   {
     name: "wind",
-    sound: "/sounds/wind.ogg",
+    sound: "/sounds/wind.mp3",
     icon: "/icons/wind.svg",
   },
 
   {
     name: "rain",
-    sound: "/sounds/rain.ogg",
+    sound: "/sounds/rain.mp3",
     icon: "/icons/rain.svg",
   },
 
   {
     name: "storm",
-    sound: "/sounds/storm.ogg",
+    sound: "/sounds/storm.mp3",
     icon: "/icons/storm.svg",
   },
 
   // {
   //   name: "stream",
-  //   sound: "/sounds/stream.ogg",
+  //   sound: "/sounds/stream.mp3",
   //   icon: "/icons/stream.svg",
   // },
 
   {
     name: "summer-night",
-    sound: "/sounds/summer-night.ogg",
+    sound: "/sounds/summer-night.mp3",
     icon: "/icons/summer-night.svg",
   },
 
   // {
   //   name: "train",
-  //   sound: "/sounds/train.ogg",
+  //   sound: "/sounds/train.mp3",
   //   icon: "/icons/train.svg",
   // },
 
   {
     name: "waves",
-    sound: "/sounds/waves.ogg",
+    sound: "/sounds/waves.mp3",
     icon: "/icons/waves.svg",
   },
 
   {
     name: "white noise",
-    sound: "/sounds/white-noise.ogg",
+    sound: "/sounds/white-noise.mp3",
     icon: "/icons/white-noise.svg",
   },
 
   {
     name: "pink-noise",
-    sound: "/sounds/pink-noise.ogg",
+    sound: "/sounds/pink-noise.mp3",
     icon: "/icons/pink-noise.svg",
   },
 ];
