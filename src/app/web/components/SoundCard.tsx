d4388668569779264f88ef91c89bfd619a4a9e84
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-10-21 13:38:07 +0100

    feat: play sound without waiting for the full audio to download

--- src/app/web/components/SoundCard.tsx
+++ src/app/web/components/SoundCard.tsx
@@ -63,6 +63,7 @@ export default function SoundCard({ item }: Props) {
               <ReactHowler
                 playing={!isMute}
                 src={sound}
+                html5={true}
                 volume={currentSoundsPlaying[name]?.volume}
                 loop={true}
               />
