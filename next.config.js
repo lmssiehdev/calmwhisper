dc90155124452d26b983ddf3bc5fdaf95fb37639
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-28 21:31:25 +0100

    fix: build

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true
-})
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+});
 
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = withPWA(nextConfig) 
+module.exports = withPWA(nextConfig);
